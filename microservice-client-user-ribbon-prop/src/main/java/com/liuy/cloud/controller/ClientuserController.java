ed5a20101c8c375fcae9902da69ea8422e641342
cnliuy <cnliuy@163.com>
2017-02-28 10:33:16 +0800

    022801

--- microservice-client-user-ribbon-prop/src/main/java/com/liuy/cloud/controller/ClientuserController.java
+++ microservice-client-user-ribbon-prop/src/main/java/com/liuy/cloud/controller/ClientuserController.java
@@ -2,6 +2,8 @@ package com.liuy.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,9 @@ public class ClientuserController {
   //实例化一个 restTemplate 	
   @Autowired
   private RestTemplate restTemplate;
+  
+  @Autowired
+  private LoadBalancerClient loadBalancerClient;
 
   //@Value("${user.userServicePath}")
   //private String userServicePath;
@@ -23,6 +28,21 @@ public class ClientuserController {
   // in ribbon need change microservice-serprovider-user-eurekaclient
   @GetMapping("/movie/{id}")
   public User findById(@PathVariable Long id) {
+
+	ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-serprovider-user-eurekaclient");
+	System.out.println("===" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
     return this.restTemplate.getForObject("http://microservice-serprovider-user-eurekaclient/simple/" + id, User.class);
+  }
+  
+  
+  @GetMapping("/test")
+  public String test() {
+    ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-serprovider-user-eurekaclient");
+    System.out.println("111" + ":" + serviceInstance.getServiceId() + ":" + serviceInstance.getHost() + ":" + serviceInstance.getPort());
+
+    ServiceInstance serviceInstance2 = this.loadBalancerClient.choose("microservice-serprovider-user-eurekaclient2");
+    System.out.println("222" + ":" + serviceInstance2.getServiceId() + ":" + serviceInstance2.getHost() + ":" + serviceInstance2.getPort());
+
+    return "1";
   }
 }
